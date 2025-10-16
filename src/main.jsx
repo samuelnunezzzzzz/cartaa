00ea0e82e0a809f4b93615cc6847d89424af13d6
alyconr <alyconr@hotmail.com>
2025-10-16 08:56:33 -0500

    deploy: trigger github pages build

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 import './index.css'
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename="/Mini-Project1-React">
+    <App />
+  </BrowserRouter>
+)
