ea5e365279a99b8deeb0a04df23fdfceecd76f83
alyconr <alyconr@hotmail.com>
2025-10-16 07:44:17 -0500

    subiendo   worklow despliegue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Mini-Project1-React/',
   plugins: [react()],
+  base: '/Mini-Project1-React/', // 👈 IMPORTANTE: nombre exacto del repo en GitHub
+  build: {
+    outDir: 'dist',              // carpeta de salida (coincide con el workflow)
+    sourcemap: true,             // opcional, útil para depuración
+  },
+  server: {
+    port: 5173,                  // opcional, puerto local
+    open: true                   // abre automáticamente el navegador al iniciar
+  }
 })
